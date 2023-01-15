dcfbe0901fe16d846fd34f6822a881a05c058644
Tomasz Widomski <widomskitomasz@gmail.com>
2023-01-15 16:42:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
-  function handleChange(event) {
-    event.preventDefault();
-    setText(event.target.value);
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
   }
-    
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea value={text} onChange={handleChange} />
-      <h4>Time reminaing: ???</h4>
-      <button>Start</button>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => setTimeRemaining(10)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
